f00281304a4fa30b6eaf783a3da721bd418b430e
webnai <wilbertboadzo144@gmail.com>
2023-03-13 00:41:31 +0000

    all tasks

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
 
-    a = tuple_a or (0, 0)
-    b = tuple_b or (0, 0)
-    if(len(a) is 1):
-        a = (tuple_a[0], 0)
-    if(len(b) is 1):
-        b = (tuple_b[0], 0)
-    return((a[0] + b[0], a[1] + b[1]))
+    if len_a == 0:
+        a1 = 0
+        a2 = 0
+    elif len_a == 1:
+        a1 = tuple_a[0]
+        a2 = 0
+    else:
+        a1 = tuple_a[0]
+        a2 = tuple_a[1]
+
+    if len_b == 0:
+        b1 = 0
+        b2 = 0
+    elif len_b == 1:
+        b1 = tuple_b[0]
+        b2 = 0
+    else:
+        b1 = tuple_b[0]
+        b2 = tuple_b[1]
+
+    new_tuple = (a1 + b1, a2 + b2)
+
+    return (new_tuple)
