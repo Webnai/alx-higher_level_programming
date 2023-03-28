940b48dd475ee7a76cfe7368ab258523ac7d7b0c
webnai <wilbertboadzo144@gmail.com>
2023-03-28 12:50:07 +0000

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,5 +7,5 @@ class Square:
     def __init__(self, size):
         """ Initializing a square class
         Args: size: size of the square
-	 """
-	self.__size = size
+         """
+        self.__size = size
