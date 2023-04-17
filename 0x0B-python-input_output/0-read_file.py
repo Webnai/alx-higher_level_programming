da0ba54c128a9c45860a887aa622b33f2f3ce4ad
webnai <wilbertboadzo144@gmail.com>
2023-04-17 19:27:32 +0000

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" read a file """
+"""a function that reads a text file (UTF8) and prints it to stdout"""
 
 
 def read_file(filename=""):
-    with open(filename,  encoding='utf-8') as f:
+    """a function that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
