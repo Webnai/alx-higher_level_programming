5368df9340a8957a4037724341364c7ac9ad756f
webnai <wilbertboadzo144@gmail.com>
2023-04-18 11:32:30 +0000

    models

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,63 +1,73 @@
 #!/usr/bin/python3
-""" class Square"""
-
-
+""" Module that contains class Square,
+inheritance of class Rectangle
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ class Square"""
+    """ Class Rectangle """
+
     def __init__(self, size, x=0, y=0, id=None):
-        """ class Square"""
+        """ Initializes instances """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """ class Square"""
-        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x,
-                self.y, self.width))
+        """ str special method """
+        str_square = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
+        return str_square + str_id + str_xy + str_wh
 
     @property
     def size(self):
-        """ class Square"""
+        """ Getter size """
         return self.width
 
     @size.setter
     def size(self, value):
-        """ class Square"""
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        """ Setter size """
         self.width = value
         self.height = value
 
+    def __str__(self):
+        """ str special method """
+        str_rectangle = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_size = "{}".format(self.size)
+
+        return str_rectangle + str_id + str_xy + str_size
+
     def update(self, *args, **kwargs):
-        """ class Square"""
-        lenofargs = len(args)
-        if lenofargs > 0:
-            self.id = args[0]
-            lenofargs -= 1
-        if lenofargs > 0:
-            self.size = args[1]
-            lenofargs -= 1
-        if lenofargs > 0:
-            self.x = args[2]
-            lenofargs -= 1
-        if lenofargs > 0:
-            self.y = args[3]
-        if kwargs:
+        """ update method """
+        if args is not None and len(args) is not 0:
+            list_atr = ['id', 'size', 'x', 'y']
+            for i in range(len(args)):
+                if list_atr[i] == 'size':
+                    setattr(self, 'width', args[i])
+                    setattr(self, 'height', args[i])
+                else:
+                    setattr(self, list_atr[i], args[i])
+        else:
             for key, value in kwargs.items():
-                if key == 'x':
-                    self.x = value
-                if key == 'y':
-                    self.y = value
                 if key == 'size':
-                    self.width = value
-                if key == 'id':
-                    self.id = value
+                    setattr(self, 'width', value)
+                    setattr(self, 'height', value)
+                else:
+                    setattr(self, key, value)
 
     def to_dictionary(self):
-        """ class Square"""
-        dic2 = {"id": self.id, "size": self.width,
-                "x": self.x, "y": self.y}
-        return dic2
+        """ Returns a dictionary with attributes """
+        list_atr = ['id', 'size', 'x', 'y']
+        dict_res = {}
+
+        for key in list_atr:
+            if key == 'size':
+                dict_res[key] = getattr(self, 'width')
+            else:
+                dict_res[key] = getattr(self, key)
+
+        return dict_res
