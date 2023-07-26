217d48d0ec56d614771c29cc3f6453c8ceb88360
Webnai <wilbertboadzo144@gmail.com>
2023-07-26 01:00:23 -0700

    its complete

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,28 +1,16 @@
 #!/usr/bin/node
-// status of a request
-const myRequest = require('request');
-const myUrl = process.argv[2];
-myRequest(myUrl, function (err, res, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    let count = 0;
-    const dict1 = {};
-    const jsonBody = JSON.parse(body);
-    if (jsonBody != null) {
-      let userant = jsonBody[0].userId;
-      for (let j = 1; j < jsonBody.length; j++) {
-        for (let i = 0; i < jsonBody.length; i++) {
-          const user = jsonBody[i].userId;
-          if (jsonBody[i].completed === true && userant === user) {
-            count = count + 1;
-            dict1[jsonBody[i].userId] = count;
-          }
-        }
-        userant = jsonBody[j].userId;
-        count = 0;
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-    }
-    console.log(dict1);
+    });
+    console.log(completed);
   }
 });
